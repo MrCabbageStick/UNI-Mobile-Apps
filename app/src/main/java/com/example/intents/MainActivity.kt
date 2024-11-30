480c8241b544a62f90e395f36c98187e5ec67142
daniel.wiszniowski <mrcabbagestick@gmail.com>
2024-12-01 00:23:29 +0100

    Mostly works

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,8 +26,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.intents.ui.theme.IntentsTheme
+import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.time.format.DateTimeParseException
+import java.util.Date
 import java.util.Locale
 
 class MainActivity : ComponentActivity() {
@@ -45,21 +51,18 @@ class MainActivity : ComponentActivity() {
                         TextField(
                             value = field1Value,
                             onValueChange = { field1Value = it },
-                            placeholder = { Text(text = "SMS number / Address / Email / Contact 1 / Event date and time") }
+                            placeholder = { Text(text = "SMS number / Address / Email / Contact 1 / Event date and time") },
+                            modifier = Modifier.fillMaxWidth().height(80.dp)
                         )
 
                         TextField(
                             value = field2Value,
                             onValueChange = { field2Value = it },
-                            placeholder = { Text(text = "SMS content / Mail title / Phone number / Contact 2 / Event title") }
+                            placeholder = { Text(text = "SMS content / Mail title / Phone number / Contact 2 / Event title") },
+                            modifier = Modifier.fillMaxWidth().height(80.dp)
                         )
 
-                        // Buttons
-
-//                        Button(onClick = { sendSMS(field1Value, field2Value) }) {
-//                            Text(text = "Send SMS")
-//                        }
-                        IntentButton(text = "Send SMS") { sendSMS(field1Value, field2Value) }
+                        IntentButton(text = "Send SMS") { sendSMS(field1Value, field2Value) } // Works
                         IntentButton(text = "Find a place") { openMap(field1Value) }
                         IntentButton(text = "Create a document") { createDocument() }
                         IntentButton(text = "Send email") { sendEmail(field1Value, field2Value) }
@@ -75,63 +78,91 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun resolve(intent: Intent, errorMessage: String): Boolean{
+        if(intent.resolveActivity(packageManager) == null){
+            Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
+            return false;
+        }
+
+        return true;
+    }
+
     // Button handlers
-    fun sendSMS(number: String, content: String){
+    private fun sendSMS(number: String, content: String){
         val intent = Intent(Intent.ACTION_SENDTO).apply {
             data = Uri.parse("sms:$number")
             putExtra("sms_body", content)
         }
 
-        startActivity(intent)
+        if(resolve(intent, "SMS application not available")){
+            startActivity(intent);
+        }
     }
 
-    fun openMap(location: String){
+    private fun openMap(location: String){
         val intent = Intent(Intent.ACTION_VIEW).apply {
             data = Uri.parse("geo:0,0?q=$location")
         }
 
-        startActivity(intent)
-    }
-
-    fun createDocument(){
-        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
-
-        if(intent.resolveActivity(packageManager) == null){
-            Toast.makeText(applicationContext, "Unable to locate document application", Toast.LENGTH_SHORT).show()
-            return
-        }
+        // For some reason it cannot resolve the intent
+        // Yet application opens properly
+//        if(resolve(intent, "Map application not available")){
+//            startActivity(intent);
+//        }
 
         startActivity(intent);
-
     }
 
-    fun sendEmail(address: String, subject: String){
+    private fun createDocument(){
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+
+        if(resolve(intent, "Unable to locate document application")){
+            startActivity(intent);
+        }
+    }
+
+    private fun sendEmail(address: String, subject: String){
         val intent = Intent(Intent.ACTION_SENDTO).apply {
-            putExtra(Intent.EXTRA_EMAIL, address)
+            type = "vnd.android.cursor.item/email"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
             putExtra(Intent.EXTRA_SUBJECT, subject)
         }
 
-        startActivity(intent)
+        if(resolve(intent, "Email app not available")) {
+            startActivity(intent)
+        }
     }
 
-    fun dial(number: String){
+    private fun dial(number: String){
         val intent = Intent(Intent.ACTION_DIAL).apply {
             data = Uri.parse("tel:$number")
         }
 
+        if(resolve(intent, "Phone application not available")){
+            startActivity(intent)
+        }
+    }
+
+    private fun launchMusicApp(){
+        val intent = Intent(Intent.ACTION_MAIN).apply{
+            addCategory(Intent.CATEGORY_APP_MUSIC)
+        }
+
         startActivity(intent)
     }
 
-    fun launchMusicApp(){
-        val intent = Intent(Intent.CATEGORY_APP_MUSIC)
-
-        startActivity(intent)
-    }
-
-    fun addCalendarEvent(title: String, dateAndTime: String){
+    private fun addCalendarEvent(title: String, dateAndTime: String){
 
         val formatter = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
-        val date = formatter.parse(dateAndTime)
+        var date: Date? = null;
+
+        try {
+            date = formatter.parse(dateAndTime)
+        }
+        catch (err: ParseException){
+            Toast.makeText(applicationContext, "Incorrect date time format", Toast.LENGTH_SHORT).show()
+            return;
+        }
 
         if(date == null){
             Toast.makeText(applicationContext, "Incorrect date time format", Toast.LENGTH_SHORT).show()
@@ -144,15 +175,19 @@ class MainActivity : ComponentActivity() {
             putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, date.time)
         }
 
-        startActivity(intent)
+        if(resolve(intent, "Unable to find a calendar application")){
+            startActivity(intent)
+        }
     }
 
-    fun openSecondActivity(contacts: List<String>){
+    private fun openSecondActivity(contacts: List<String>){
         val intent = Intent(this, SecondActivity::class.java).apply {
             putExtra("contacts", contacts.toTypedArray())
         }
 
-        startActivity(intent)
+        if(resolve(intent, "Unable to launch seconds activity")){
+            startActivity(intent)
+        }
     }
 
 }
